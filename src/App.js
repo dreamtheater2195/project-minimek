e45838b14c75e0b90bc82ee31b8cead14ef73794
Quan Minh Pham <dreamtheater2195@gmail.com>
2018-07-29 22:41:32 +0700

    Add basic TabBar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {
+  Header,
+  Container,
+  Menu,
+} from "semantic-ui-react";
 import './App.css';
-
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-header">
+          <Header inverted as="h1">Project Mini-Mek</Header>
+        </div>
+        <Container>
+          <Menu tabular size="massive">
+            <Menu.Item name="unitInfo" active={true}>Unit Info</Menu.Item>
+            <Menu.Item name="pilots" active={false}>Pilots</Menu.Item>
+            <Menu.Item name="mechs" active={false}>Mechs</Menu.Item>
+            <Menu.Item name="unitOrganization" active={false}>Unit Organization</Menu.Item>
+          </Menu>
+        </Container>
       </div>
+
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
+import "semantic-ui-css/semantic.css";
+const store = configureStore();
+const rootEl = document.getElementById('root');
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+let render = () => {
+    const App = require('./App').default;
+    ReactDOM.render(
+        <Provider store={store}>
+            <App />
+        </Provider>,
+        rootEl
+    );
+}
+if (module.hot) {
+    const renderApp = render;
+    const renderError = (error) => {
+        const RedBox = require('redbox-react').default;
+        ReactDOM.render(
+            <RedBox error={error} />,
+            rootEl
+        )
+    };
+    render = () => {
+        try {
+            renderApp();
+        } catch (err) {
+            renderError(err);
+        }
+    }
+    module.hot.accept('./App', () => {
+        setTimeout(render);
+    })
+}
+
+render();
